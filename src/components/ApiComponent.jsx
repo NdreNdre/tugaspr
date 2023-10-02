e2c21f3e51e1c318b47e7391e5a3e85b07ec0132
NdreNdre <andresahulata08@gmail.com>
2023-10-02 13:54:47 +0700

    third commit

--- src/components/ApiComponent.jsx
+++ src/components/ApiComponent.jsx
@@ -1,16 +1,18 @@
-import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import { Container, Row, Col, Form, Button ,FormControl} from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from 'react'
 import { useDebounce } from 'use-debounce'
+import axios from 'axios';
 
 
 function ApiComponent(){
 let navigate = useNavigate();
 
-    const [users,setUsers] = useState([]);
-    const [search, setSearch] = useState("");
-    const [debounceValue] = useDebounce(search, 3000);
-    console.log(debounceValue);
+const [users,setUsers] = useState([]);
+const [userDetail, setUserDetail] = useState([]);
+const [search, setSearch] = useState("");
+const [debounceValue] = useDebounce(search, 3000);
+const [value, setValue] = useState("");
 
 
     const getUser = async () => {
@@ -19,9 +21,26 @@ let navigate = useNavigate();
         setUsers(FinalData);
     }
 
+    const getUserDetail = async () => {
+        const res = await axios.get(`https://api.github.com/users/${value}`)
+        return res.data;
+    }
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        setValue(e.target.datauser.value);
+    }
+
     useEffect(() => {
         getUser();
     }, [])
+
+    useEffect(() => {
+        getUserDetail().then((res) =>{
+            setUserDetail(res);
+        });
+    }, [value])
+
 
 
     return (
@@ -46,12 +65,14 @@ let navigate = useNavigate();
                                     </Form.Control>
                                 </Col>
                             </Row>
-                            <Row>
-                                <Col>
+                            </Form.Group>
+                    </Form>
+                    <Row>
+                                <Col className="user-list col-xl-4 col-md-7 col-sm-10">
                                 <table className="tbl-main text-light">
                                     <thead>
                                         <tr>
-                                        <th>Profile Pict</th>
+                                        <th>Gambar</th>
                                         <th>Username</th>
                                         </tr>
                                     </thead>
@@ -61,19 +82,61 @@ let navigate = useNavigate();
                                         return search === '' ? data : data.login.includes(search);  
                                         }).map((data) => {
                                         return (
+                                            
                                             <tr key={data.id}>
+                                                
                                             <td><img src={data.avatar_url} alt="gambar user" className='gambarMuka'/></td>
-                                            <td>{data.login}</td>
+                                            <td>{data.login}
+                                            <Form onSubmit={handleSubmit}>
+                                                <Form.Group>
+                                                <FormControl type="hidden" name="datauser" defaultValue={data.login}></FormControl>
+                                                <button>Klik Untuk Detail</button>
+                                                </Form.Group>
+                                                </Form></td>
                                             </tr>
                                         )
                                         })
                                     }
+                                    
                                     </tbody>
                                 </table>
                                 </Col>
+                                <Col className="user-detail">
+                                <div className="user-detail-canvas">
+                                    <Row>
+                                        <Col>
+                                        <h2>User Detail</h2>
+                                        </Col>
+
+                                    </Row>
+                                    <Row>
+                                        <Col className="gambarmukadetail">
+                                        <img src={userDetail.avatar_url} alt="" />
+                                        </Col>
+                                        <Col>
+                                        <p>Username</p>
+                                        <p>Nama Lengkap</p>
+                                        <p>Lokasi</p>
+                                        <p>Repository</p>
+                                        <p>Following</p>
+                                        <p>Follower</p>
+                                        <p>Blog</p>
+                                        </Col>
+                                        <Col>
+                                        <p>: {userDetail.login}</p>
+                                        <p>: {userDetail.name}</p>
+                                        <p>: {userDetail.location}</p>
+                                        <p>: {userDetail.public_repos}</p>
+                                        <p>: {userDetail.following}</p>
+                                        <p>: {userDetail.followers}</p>
+                                        <p>: {userDetail.blog}</p>
+                                        </Col>
+                                    </Row>
+
+                                    </div>
+                                </Col>
                             </Row>
-                        </Form.Group>
-                    </Form>
+                        
                 </div>
             </Container>    
         </div>
